aac1a26ace709fa49dd8d8763979c4f782a6fe94
himanshumishra1309 <himanshu.mishra0913@gmail.com>
2025-12-29 15:56:32 +0530

    initialized frontend and added utils to handle response better and added another function in the scraping script

--- server/src/model/article.model.js
+++ server/src/model/article.model.js
@@ -3,28 +3,54 @@ import mongoose, {Schema} from "mongoose";
 const articleSchema = new Schema({
     title:{
         type: String,
-        index: true,
+        required: true,
+        trim: true,
+        maxlength: 200,
+        index: true
+    },
+    slug:{
+        type: String,
+        unique: true,
+        lowercase: true,
     },
     blogUrl:{
         type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
     imageUrl:{
         type: String,
+        trim: true
     },
     author:{
         type: String,
+        required: true,
+        trim: true,
+        default: 'Unknown'
     },
     publishedDate:{
-        type: String,
+        type: Date,
+        index: true
     },
     excerpt:{
         type: String,
+        trim: true,
     },
-    content: {
+    contentText: {
         type: String,
     },
-    source:{
+    contentHtml:{
         type: String,
+    },
+    contentImages:{
+        type: [String],
+        default: []
+    },
+    tags: {
+        type: [String],
+        default: [],
+        index: true
     },
     status: {
         type: String,
--- server/src/scripts/beyondChatScrape.js
+++ server/src/scripts/beyondChatScrape.js
@@ -1,54 +1,101 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
+const blogs = [];
+
 async function getMaxPageNumbers() {
-  const { data } = await axios.get("https://beyondchats.com/blogs/");
-  const $ = cheerio.load(data);
-
-  const pages = [];
-
-  $(".page-numbers").each((_, el) => {
-    const page = parseInt($(el).text());
-    if (!isNaN(page)) pages.push(page);
-  });
-
-  return Math.max(...pages);
+  try {
+    const { data } = await axios.get("https://beyondchats.com/blogs/");
+    const $ = cheerio.load(data);
+  
+    const pages = [];
+  
+    $(".page-numbers").each((_, el) => {
+      const page = parseInt($(el).text());
+      if (!isNaN(page)) pages.push(page);
+    });
+  
+    return Math.max(...pages);
+  } catch (error) {
+    
+  }
 }
 
 async function getFiveBlogs() {
-  const lastPage = await getMaxPageNumbers();
-  let page = lastPage;
-  const blogs = [];
+  try {
+    const lastPage = await getMaxPageNumbers();
+    let page = lastPage;
 
-  while (blogs.length < 5 && page > 0) {
-    const url = `https://beyondchats.com/blogs/page/${page}/`;
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+    while (blogs.length < 5 && page > 0) {
+      const url = `https://beyondchats.com/blogs/page/${page}/`;
+      const { data } = await axios.get(url);
+      const $ = cheerio.load(data);
+  
+      $("article").each((_, el) => {
+        if (blogs.length < 5) {
+          const textTitle = $(el).find("h2.entry-title a");
+          const title = textTitle.text().trim();
+          const blogUrl = textTitle.attr("href");
+          const blogUrlParts = blogUrl.split('/').filter(Boolean);
+          const slug = blogUrlParts[blogUrlParts.length - 1];
+          const imageUrl = $(el).find("a.ct-media-container img").attr("src");
+          const author = $(el).find("li.meta-author span").text().trim();
+          const publishedDate = $(el).find("li.meta-date time").attr("datetime");
+          const excerpt = $(el).find("div.entry-excerpt p").text().trim();
+          const tags = $(el).find("li.meta-categories a")
+                  .map((_, tag) => $(tag).text().trim()).get();
+  
+          blogs.push({
+              title,
+              slug,
+              blogUrl,
+              imageUrl,
+              author,
+              publishedDate,
+              excerpt,
+              tags
+          });
+        }
+      });
+  
+      page--;
+    }
+  } catch (error) {
+    
+  }
+}
 
-    $("article").each((_, el) => {
-      if (blogs.length < 5) {
-        const textTitle = $(el).find("h2.entry-title a");
-        const title = textTitle.text().trim();
-        const blogUrl = textTitle.attr("href");
-        const imageUrl = $(el).find("a.ct-media-container img").attr("src");
-        const author = $(el).find("li.meta-author span").text().trim();
-        const publishedDate = $(el).find("li.meta-date time").attr("datetime");
-        const entryExcerpt = $(el).find("div.entry-excerpt p").text().trim();
-        const tags = $(el).find("li.meta-categories a")
-                .map((_, tag) => $(tag).text().trim()).get();
+async function getBlogData() {
+  try {
+    await Promise.all(
+      blogs.map(async (el) => {
+        const url = el.blogUrl;
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
 
-        blogs.push({
-            title,
-            blogUrl,
-            imageUrl,
-            author,
-            publishedDate,
-            entryExcerpt,
-            tags
+        $(".wp-applause-container").remove();
+        $(".elementor-share-buttons").remove();
+        $(".has-social-placeholder").remove();
+
+        const contentText = $(".elementor-widget-theme-post-content")
+          .text()
+          .replace(/\s+/g, " ")
+          .trim();
+
+        const contentHtml = $(".elementor-widget-theme-post-content").html();
+
+        const contentImages = [];
+        $(".elementor-widget-theme-post-content img").each((_, img) => {
+          const src = $(img).attr("src");
+          if (src) contentImages.push(src);
         });
-      }
-    });
 
-    page--;
+        el.contentText = contentText;
+        el.contentHtml = contentHtml;
+        el.contentImages = contentImages;
+      })
+    );
+  } catch (error) {
+    console.error(error);
   }
 }
